882784d6fd5ec59ae0027e2c654631b4082b4ff0
sskaren1 <karen.93st@gmail.com>
2024-04-16 16:59:13 -0500

    add: creando el state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { categories } from "../data/categories";
 
 export default function Form() {
+  const [activity, setActivity] = useState({
+    category: 1,
+    name: "",
+    calories: 0,
+  });
+
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
       <div className="grid grid-cols-1 gap-3">
@@ -10,6 +17,7 @@ export default function Form() {
         <select
           className="border border-slate-300 p-2 rounded-lg w-full bg-white"
           id="category"
+          value={activity.category}
         >
           {categories.map((category) => (
             <option key={category.id} value={category.id}>
@@ -28,8 +36,10 @@ export default function Form() {
           type="text"
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+          value={activity.name}
         />
       </div>
+
       <div className="grid grid-cols-1 gap-3">
         <label htmlFor="calories" className="font-bold">
           Calorias:
@@ -39,8 +49,10 @@ export default function Form() {
           type="number"
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Calorias. ej. 300 o 500"
+          value={activity.calories}
         />
       </div>
+
       <input
         type="submit"
         className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
