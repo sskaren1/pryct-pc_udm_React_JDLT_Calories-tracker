c0d5ba3aa5be16e2114ea54224bcd4a4697ca2a0
sskaren1 <karen.93st@gmail.com>
2024-04-16 16:07:15 -0500

    add: creando contenedores principales

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import Form from "./components/Form";
 
 function App() {
-
   return (
     <>
-      <h1 className="text-6xl">Contador de calorias</h1>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto fles justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase">
+            Contador de Calorías
+          </h1>
+        </div>
+      </header>
+
+      <section className="bg-lime-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <Form/>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
